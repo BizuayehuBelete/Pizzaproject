d5e5c233ce2794eb214428a0b48ccb7465632971
BizuayehuBelete <118741898+bizuayehubelete@users.noreply.github.com>
2022-11-27 02:11:48 +0200

    Pizza Project

---
+++ choose.py
@@ -0,0 +1,6 @@
+def choose():
+    customer = input("do you want to contnue? y/n ")
+    if customer == 'n':
+        exit()
+        return customer
+choose()
---
+++ main.py
@@ -0,0 +1,10 @@
+from diagram import diagrame
+from utility import *
+from unit import main1
+
+diagrame()
+main1()
+
+
+
+
